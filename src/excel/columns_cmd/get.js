4c649461087c1b47cd56cdc2affa120e438d38f0
Dominique Pfister <dominique.pfister@gmail.com>
2020-06-27 11:27:04 +0200

    feat(excel): add columns command (#53)

---
+++ src/excel/columns_cmd/get.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+'use strict';
+
+const chalk = require('chalk');
+const { info } = require('@adobe/helix-log');
+
+const {
+  loadState, getOneDriveClient,
+} = require('../../client.js');
+
+function getDriveItem(url) {
+  // todo: parse better
+  const [, , driveId, , id] = url.split('/');
+  return {
+    id,
+    parentReference: {
+      driveId,
+    },
+  };
+}
+
+async function handler(args) {
+  const tableName = args['table-name'];
+  const columnName = args['column-name'];
+
+  const state = await loadState();
+  if (!state.root) {
+    throw Error(chalk`${args._[0]} needs path. use '{grey ${args.$0} resolve}' to set root.`);
+  }
+
+  const od = getOneDriveClient();
+  const driveItem = getDriveItem(state.root);
+  const workbook = await od.getWorkbook(driveItem);
+
+  const values = await workbook.table(tableName).getColumn(columnName);
+  values
+    .forEach((row) => {
+      info(chalk`${row}`);
+    });
+}
+
+Object.assign(exports, {
+  command: ['get <table-name> <column-name>'],
+  desc: 'Retrieve a column in a table',
+  handler: (y) => handler(y),
+});
